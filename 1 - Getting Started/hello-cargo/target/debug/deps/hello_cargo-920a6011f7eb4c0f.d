acff8c3924a769c7fe85fd0199d087d8d02110ef
pulakk <pulakkuli72@gmail.com>
2020-05-19 15:42:53 +0530

    removing debug files from chapter 1

--- 1 - Getting Started/hello-cargo/target/debug/deps/hello_cargo-920a6011f7eb4c0f.d
+++
@@ -1,5 +0,0 @@
-/Users/pulak/play/git/learning-rust/1 - Getting Started/hello-cargo/target/debug/deps/hello_cargo-920a6011f7eb4c0f: src/main.rs
-
-/Users/pulak/play/git/learning-rust/1 - Getting Started/hello-cargo/target/debug/deps/hello_cargo-920a6011f7eb4c0f.d: src/main.rs
-
-src/main.rs:
--- 1 - Getting Started/hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/pulak/play/git/learning-rust/1\ -\ Getting\ Started/hello-cargo/target/debug/hello-cargo: /Users/pulak/play/git/learning-rust/1\ -\ Getting\ Started/hello-cargo/src/main.rs
